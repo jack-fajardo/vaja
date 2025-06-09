d26235470749191aa5c5122648cc823bc7c1a991
jack-fajardo <jackfajardotech@gmail.com>
2025-06-09 11:25:53 +0800

    generated a spring project

---
+++ src/main/java/com/example/vaja/VajaApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vaja/VajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
